ansible-collections/cisco.iosxr

commit a93b8c54dc4f730d03206d1ee57d512ea1515ac5

added new states
Signed-off-by: GomathiselviS <gomathiselvi@gmail.com>

plugins/module_utils/network/iosxr/config/lag_interfaces/lag_interfaces.py
@@ -51,14 +51,14 @@ class Lag_interfaces(ConfigBase):
     def __init__(self, module):
         super(Lag_interfaces, self).__init__(module)
 
-    def get_lag_interfaces_facts(self):
+    def get_lag_interfaces_facts(self, data=None):
         """ Get the 'facts' (the current configuration)
 
         :rtype: A dictionary
         :returns: The current configuration as a dictionary
         """
         facts, _warnings = Facts(self._module).get_facts(
-            self.gather_subset, self.gather_network_resources
+            self.gather_subset, self.gather_network_resources, data=data
         )
         lag_interfaces_facts = facts["ansible_network_resources"].get(
             "lag_interfaces"
@@ -77,19 +77,43 @@ class Lag_interfaces(ConfigBase):
         warnings = list()
         commands = list()
 
-        existing_lag_interfaces_facts = self.get_lag_interfaces_facts()
-        commands.extend(self.set_config(existing_lag_interfaces_facts))
-        if commands:
+        if self.state in self.ACTION_STATES:
+            existing_lag_interfaces_facts = self.get_lag_interfaces_facts()
+        else:
+            existing_lag_interfaces_facts = {}
+
+        if self.state in self.ACTION_STATES or self.state == "rendered":
+            commands.extend(self.set_config(existing_lag_interfaces_facts))
+
+        if commands and self.state in self.ACTION_STATES:
             if not self._module.check_mode:
                 self._connection.edit_config(commands)
             result["changed"] = True
-        result["commands"] = commands
 
-        changed_lag_interfaces_facts = self.get_lag_interfaces_facts()
+        if self.state in self.ACTION_STATES:
+            result["commands"] = commands
 
-        result["before"] = existing_lag_interfaces_facts
-        if result["changed"]:
-            result["after"] = changed_lag_interfaces_facts
+        if self.state in self.ACTION_STATES or self.state == "gathered":
+            changed_lag_interfaces_facts = self.get_lag_interfaces_facts()
+
+        elif self.state == "rendered":
+            result["rendered"] = commands
+
+        elif self.state == "parsed":
+            running_config = self._module.params["running_config"]
+            if not running_config:
+                self._module.fail_json(
+                    msg="value of running_config parameter must not be empty for state parsed"
+                )
+            result["parsed"] = self.get_lag_interfaces_facts(data=running_config)
+
+        if self.state in self.ACTION_STATES:
+            result["before"] = existing_lag_interfaces_facts
+            if result["changed"]:
+                result["after"] = changed_lag_interfaces_facts
+
+        elif self.state == "gathered":
+            result["gathered"] = changed_lag_interfaces_facts
 
         result["warnings"] = warnings
         return result
@@ -130,7 +154,7 @@ class Lag_interfaces(ConfigBase):
         state = self._module.params["state"]
         commands = []
 
-        if state in ("overridden", "merged", "replaced") and not want:
+        if self.state in ("merged", "replaced", "rendered") and not want:
             self._module.fail_json(
                 msg="value of config parameter must not be empty for state {0}".format(
                     state
@@ -152,7 +176,7 @@ class Lag_interfaces(ConfigBase):
                 name = item["name"]
                 obj_in_have = search_obj_in_list(name, have)
 
-                if state == "merged":
+                if state in ("merged", "rendered"):
                     commands.extend(self._state_merged(item, obj_in_have))
 
                 elif state == "replaced":

plugins/module_utils/network/iosxr/argspec/lag_interfaces/lag_interfaces.py
@@ -71,8 +71,16 @@ class Lag_interfacesArgs(object):  # pylint: disable=R0903
             },
             "type": "list",
         },
+        "running_config": {"type": "str"},
         "state": {
-            "choices": ["merged", "replaced", "overridden", "deleted"],
+            "choices": [
+                "merged",
+                "replaced",
+                "deleted",
+                "parsed",
+                "rendered",
+                "gathered",
+            ],
             "default": "merged",
             "type": "str",
         },

plugins/modules/iosxr_lag_interfaces.py
@@ -37,9 +37,10 @@ ANSIBLE_METADATA = {
 }
 
 DOCUMENTATION = """module: iosxr_lag_interfaces
-short_description: Manages attributes of LAG/Ether-Bundle interfaces on IOS-XR devices.
+short_description: Lag Interfaces resiurce module.
 description:
 - This module manages the attributes of LAG/Ether-Bundle interfaces on IOS-XR devices.
+version_added: 1.0.0
 notes:
 - Tested against IOS-XR 6.1.3.
 - This module works with connection C(network_cli). See L(the IOS-XR Platform Options,../network/user_guide/platform_iosxr.html).
@@ -115,6 +116,15 @@ options:
         choices:
         - dst-ip
         - src-ip
+  running_config:
+    description:
+      - This option is used only with state I(parsed).
+      - The value of this option should be the output received from the IOS-XR device by executing
+        the command B(show running-config int).
+      - The state I(parsed) reads the configuration from C(running_config) option and transforms
+        it into Ansible structured data as per the resource module's argspec and the value is then
+        returned in the I(parsed) key within the result.
+    type: str
   state:
     description:
     - The state of the configuration after module completion.
@@ -124,6 +134,9 @@ options:
     - replaced
     - overridden
     - deleted
+    - parsed
+    - rendered
+    - gathered
     default: merged
 """
 EXAMPLES = """
@@ -158,7 +171,7 @@ EXAMPLES = """
 #
 #
 - name: Merge provided configuration with device configuration
-  iosxr_lag_interfaces:
+  cisco.iosxr.iosxr_lag_interfaces:
     config:
       - name: Bundle-Ether10
         members:
@@ -268,7 +281,7 @@ EXAMPLES = """
 #
 #
 - name: Replace device configuration of listed Bundles with provided configurations
-  iosxr_lag_interfaces:
+  cisco.iosxr.iosxr_lag_interfaces:
     config:
       - name: Bundle-Ether12
         members:
@@ -377,7 +390,7 @@ EXAMPLES = """
 #
 
 - name: Overrides all device configuration with provided configuration
-  iosxr_lag_interfaces:
+  cisco.iosxr.iosxr_lag_interfaces:
     config:
       - name: Bundle-Ether10
         members:
@@ -478,7 +491,7 @@ EXAMPLES = """
 #
 
 - name: Delete attributes of given bundles and removes member interfaces from them (Note - This won't delete the bundles themselves)
-  iosxr_lag_interfaces:
+  cisco.iosxr.iosxr_lag_interfaces:
     config:
       - name: Bundle-Ether10
       - name: Bundle-Ether11
@@ -564,7 +577,7 @@ EXAMPLES = """
 #
 
 - name: Delete attributes of all bundles and removes member interfaces from them (Note - This won't delete the bundles themselves)
-  iosxr_lag_interfaces:
+  cisco.iosxr.iosxr_lag_interfaces:
     state: deleted
 
 #
@@ -598,6 +611,197 @@ EXAMPLES = """
 # interface GigabitEthernet0/0/0/4
 #  shutdown
 # !
+
+# Using parsed:
+
+# parsed.cfg
+
+# interface Bundle-Ether10
+#  lacp mode active
+#  bundle load-balancing hash src-ip
+#  bundle maximum-active links 5
+#  bundle minimum-active links 2
+# !
+# interface Bundle-Ether12
+#  bundle load-balancing hash dst-ip
+# !
+# interface Loopback888
+#  description test for ansible
+#  shutdown
+# !
+# interface MgmtEth0/0/CPU0/0
+#  ipv4 address 192.0.2.11 255.255.255.0
+# !
+# interface GigabitEthernet0/0/0/1
+#  description 'GigabitEthernet - 1"
+#  bundle id 10 mode inherit
+# !
+# interface GigabitEthernet0/0/0/2
+#  description "GigabitEthernet - 2"
+#   bundle id 12 mode passive
+# !
+# interface GigabitEthernet0/0/0/3
+#  description "GigabitEthernet - 3"
+#  bundle id 10 mode inherit
+# !
+# interface GigabitEthernet0/0/0/4
+#  description "GigabitEthernet - 4"
+#  bundle id 12 mode passive
+# !
+#
+- name: Convert lag interfaces config to argspec without connecting to the appliance
+    cisco.iosxr.iosxr_lag_interfaces:
+      running_config: "{{ lookup('file', './parsed.cfg') }}"
+
+# --------------
+# Output
+# --------------
+#   parsed:
+#     - name: Bundle-Ether10
+#       members:
+#         - member: GigabitEthernet0/0/0/1
+#           mode: inherit
+#         - member: GigabitEthernet0/0/0/3
+#           mode: inherit
+#       mode: active
+#       links:
+#         max_active: 5
+#         min_active: 2
+#       load_balancing_hash: src-ip
+
+#     - name: Bundle-Ether12
+#       members:
+#         - member: GigabitEthernet0/0/0/2
+#           mode: passive
+#         - member: GigabitEthernet0/0/0/4
+#           mode: passive
+#       load_balancing_hash: dst-ip
+
+# using gathered
+
+# Device Config:
+# -------------
+
+# interface Bundle-Ether10
+#  lacp mode active
+#  bundle load-balancing hash src-ip
+#  bundle maximum-active links 5
+#  bundle minimum-active links 2
+# !
+# interface Bundle-Ether12
+#  bundle load-balancing hash dst-ip
+# !
+# interface Loopback888
+#  description test for ansible
+#  shutdown
+# !
+# interface MgmtEth0/0/CPU0/0
+#  ipv4 address 192.0.2.11 255.255.255.0
+# !
+# interface GigabitEthernet0/0/0/1
+#  description 'GigabitEthernet - 1"
+#  bundle id 10 mode inherit
+# !
+# interface GigabitEthernet0/0/0/2
+#  description "GigabitEthernet - 2"
+#   bundle id 12 mode passive
+# !
+# interface GigabitEthernet0/0/0/3
+#  description "GigabitEthernet - 3"
+#  bundle id 10 mode inherit
+# !
+# interface GigabitEthernet0/0/0/4
+#  description "GigabitEthernet - 4"
+#  bundle id 12 mode passive
+# !
+#
+
+- name: Gather IOSXR lag interfaces configuration
+  cisco.iosxr.iosxr_lag_interfaces:
+    config:
+    state: gathered
+
+# --------------
+# Output
+# --------------
+#   gathered:
+#     - name: Bundle-Ether10
+#       members:
+#         - member: GigabitEthernet0/0/0/1
+#           mode: inherit
+#         - member: GigabitEthernet0/0/0/3
+#           mode: inherit
+#       mode: active
+#       links:
+#         max_active: 5
+#         min_active: 2
+#       load_balancing_hash: src-ip
+
+#     - name: Bundle-Ether12
+#       members:
+#         - member: GigabitEthernet0/0/0/2
+#           mode: passive
+#         - member: GigabitEthernet0/0/0/4
+#           mode: passive
+#       load_balancing_hash: dst-ip
+
+# Using rendered:
+- name: Render platform specific commands from task input using rendered state
+  cisco.iosxr.iosxr_lag_interfaces:
+    config:
+        - name: Bundle-Ether10
+          members:
+          - member: GigabitEthernet0/0/0/1
+            mode: inherit
+          - member: GigabitEthernet0/0/0/3
+            mode: inherit
+        mode: active
+        links:
+          max_active: 5
+          min_active: 2
+        load_balancing_hash: src-ip
+
+      - name: Bundle-Ether12
+        members:
+          - member: GigabitEthernet0/0/0/2
+            mode: passive
+          - member: GigabitEthernet0/0/0/4
+            mode: passive
+        load_balancing_hash: dst-ip
+    state: rendered
+
+# Output:
+
+# rendered:
+#    [
+#         - "interface Bundle-Ether10"
+#         - " lacp mode active"
+#         - " bundle load-balancing hash src-ip"
+#         - " bundle maximum-active links 5"
+#         - " bundle minimum-active links 2"
+#         - "interface Bundle-Ether12"
+#         - " bundle load-balancing hash dst-ip"
+#         - "interface Loopback888"
+#         - " description test for ansible"
+#         - " shutdown"
+#         - "interface MgmtEth0/0/CPU0/0"
+#         - " ipv4 address 192.0.2.11 255.255.255.0"
+#         - "interface GigabitEthernet0/0/0/1"
+#         - " description 'GigabitEthernet - 1""
+#         - " bundle id 10 mode inherit"
+#         - "interface GigabitEthernet0/0/0/2"
+#         - " description "GigabitEthernet - 2""
+#         - "  bundle id 12 mode passive"
+#         - "interface GigabitEthernet0/0/0/3"
+#         - " description "GigabitEthernet - 3""
+#         - " bundle id 10 mode inherit"
+#         - "interface GigabitEthernet0/0/0/4"
+#         - " description "GigabitEthernet - 4""
+#         - " bundle id 12 mode passive"
+#    ]
+#
+#
+
 
 """
 RETURN = """
@@ -642,11 +846,16 @@ def main():
         ("state", "merged", ("config",)),
         ("state", "replaced", ("config",)),
         ("state", "overridden", ("config",)),
+        ("state", "rendered", ("config",)),
+        ("state", "parsed", ("running_config",)),
     ]
+
+    mutually_exclusive = [("config", "running_config")]
     module = AnsibleModule(
         argument_spec=Lag_interfacesArgs.argument_spec,
         required_if=required_if,
         supports_check_mode=True,
+        mutually_exclusive=mutually_exclusive,
     )
 
     result = Lag_interfaces(module).execute_module()

tests/integration/targets/iosxr_lag_interfaces/tests/cli/empty_config.yaml
@@ -35,3 +35,26 @@
 - assert:
     that:
       - result.msg == 'value of config parameter must not be empty for state overridden'
+
+- name: Rendered with empty config should give appropriate error message
+  register: result
+  ignore_errors: true
+  cisco.iosxr.iosxr_lag_interfaces:
+    config:
+    state: rendered
+
+- assert:
+    that:
+      - result.msg == 'value of config parameter must not be empty for state rendered'
+
+- name: Parsed with empty config should give appropriate error message
+  register: result
+  ignore_errors: true
+  cisco.iosxr.iosxr_lag_interfaces:
+    running_config:
+    state: parsed
+
+- assert:
+    that:
+      - result.msg == 'value of running_config parameter must not be empty for state
+        parsed'

tests/integration/targets/iosxr_lag_interfaces/tests/cli/fixtures/parsed.cfg
@@ -1,0 +1,33 @@
+interface Bundle-Ether10
+ lacp mode active
+ bundle load-balancing hash src-ip
+ bundle maximum-active links 5
+ bundle minimum-active links 2
+!
+interface Bundle-Ether12
+ bundle load-balancing hash dst-ip
+!
+interface Loopback888
+ description test for ansible
+ shutdown
+!
+interface MgmtEth0/0/CPU0/0
+ ipv4 address 192.0.2.11 255.255.255.0
+!
+interface GigabitEthernet0/0/0/1
+ description 'GigabitEthernet - 1"
+ bundle id 10 mode inherit
+!
+interface GigabitEthernet0/0/0/2
+ description "GigabitEthernet - 2"
+  bundle id 12 mode passive
+!
+interface GigabitEthernet0/0/0/3
+ description "GigabitEthernet - 3"
+ bundle id 10 mode inherit
+!
+interface GigabitEthernet0/0/0/4
+ description "GigabitEthernet - 4"
+ bundle id 12 mode passive
+!
+

tests/integration/targets/iosxr_lag_interfaces/tests/cli/gathered.yaml
@@ -1,0 +1,49 @@
+---
+- debug:
+    msg: START iosxr_lag_interfaces gathered integration tests on connection={{
+      ansible_connection }}
+
+- include_tasks: _remove_config.yaml
+
+- block:
+
+    - name: Gather the provided configuration with the existing running configuration
+      register: result
+      cisco.iosxr.iosxr_lag_interfaces:
+        config:
+          - name: Bundle-Ether10
+            mode: active
+            members:
+
+              - member: GigabitEthernet0/0/0/0
+                mode: inherit
+
+              - member: GigabitEthernet0/0/0/1
+                mode: passive
+            load_balancing_hash: src-ip
+            links:
+              max_active: 10
+              min_active: 2
+
+          - name: Bundle-Ether11
+            load_balancing_hash: dst-ip
+            members:
+
+              - member: GigabitEthernet0/0/0/8
+                mode: passive
+
+              - member: GigabitEthernet0/0/0/9
+                mode: passive
+        state: merged
+
+    - name: Gather LACP facts using gathered state
+      register: result
+      cisco.iosxr.iosxr_lag_interfaces:
+        state: gathered
+
+    - name: Assert that facts were correctly generated
+      assert:
+        that: "{{ merged['after'] == result['gathered'] }}"
+  always:
+
+    - include_tasks: _remove_config.yaml

tests/integration/targets/iosxr_lag_interfaces/tests/cli/parsed.yaml
@@ -1,0 +1,15 @@
+---
+- debug:
+    msg: START iosxr_lag_interfaces parsed integration tests on connection={{ ansible_connection
+      }}
+
+- name: Parse externally provided lag interfaces config to agnostic model
+  register: result
+  cisco.iosxr.iosxr_lag_interfaces:
+    running_config: "{{ lookup('file', './fixtures/parsed.cfg') }}"
+    state: parsed
+
+- name: Assert that config was correctly parsed
+  assert:
+    that:
+      - "{{ parsed['after'] == result['parsed'] }}"

tests/integration/targets/iosxr_lag_interfaces/tests/cli/rendered.yaml
@@ -1,0 +1,35 @@
+---
+- debug:
+    msg: START iosxr_lag_interfaces rendered integration tests on connection={{
+      ansible_connection }}
+
+- name: Render platform specific commands from task input using rendered state
+  register: result
+  cisco.iosxr.iosxr_lag_interfaces:
+    config:
+      - name: Bundle-Ether10
+        members:
+          - member: GigabitEthernet0/0/0/1
+            mode: inherit
+          - member: GigabitEthernet0/0/0/3
+            mode: inherit
+        mode: active
+        links:
+          max_active: 5
+          min_active: 2
+        load_balancing_hash: src-ip
+
+      - name: Bundle-Ether12
+        members:
+          - member: GigabitEthernet0/0/0/2
+            mode: passive
+          - member: GigabitEthernet0/0/0/4
+            mode: passive
+        load_balancing_hash: dst-ip
+    state: rendered
+
+- name: Assert that correct set of commands were rendered
+  assert:
+    that:
+      - "{{ rendered['commands'] | symmetric_difference(result['rendered']) |length\
+        \ == 0 }}"

commit 8a5ac9202126abd747b67aa17b6c9377b7a53518

lag_interfaces.py - fix bug caused by commit a93b8c54dc4f730d03206d1ee57d512ea1515ac5

plugins/module_utils/network/iosxr/config/lag_interfaces/lag_interfaces.py
@@ -130,7 +130,10 @@ class Lag_interfaces(ConfigBase):
         state = self._module.params["state"]
         commands = []
 
-        if state in ("overridden", "merged", "replaced") and not want:
+        if (
+            state in ("overridden", "merged", "replaced", "rendered")
+            and not want
+        ):
             self._module.fail_json(
                 msg="value of config parameter must not be empty for state {0}".format(
                     state