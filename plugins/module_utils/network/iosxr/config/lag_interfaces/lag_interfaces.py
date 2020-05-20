180fd22f3ab2ea576ae9c57355f742715811ef1f
Viktor Velichkin <v.velichkin@corp.mail.ru>
2020-05-20 22:17:05 +0300

    Revert "lag_interfaces.py - fix bug caused by commit a93b8c54dc4f730d03206d1ee57d512ea1515ac5"
    This reverts commit 8a5ac9202126abd747b67aa17b6c9377b7a53518.

--- plugins/module_utils/network/iosxr/config/lag_interfaces/lag_interfaces.py
+++ plugins/module_utils/network/iosxr/config/lag_interfaces/lag_interfaces.py
@@ -156,10 +156,7 @@ class Lag_interfaces(ConfigBase):
         state = self._module.params["state"]
         commands = []
 
-        if (
-            state in ("overridden", "merged", "replaced", "rendered")
-            and not want
-        ):
+        if state in ("overridden", "merged", "replaced") and not want:
             self._module.fail_json(
                 msg="value of config parameter must not be empty for state {0}".format(
                     state
