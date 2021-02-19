97d7f88343f660cf85415f19470b3e46e4cc8f52
ashwini-mhatre <mashu97@gmail.com>
2021-02-19 11:54:12 +0530

    fix uts for newly added module

--- tests/unit/modules/network/iosxr/test_iosxr_bgp_global.py
+++ tests/unit/modules/network/iosxr/test_iosxr_bgp_global.py
@@ -37,7 +37,7 @@ class TestIosxrBgpGlobalModule(TestIosxrModule):
         super(TestIosxrBgpGlobalModule, self).setUp()
 
         self.mock_get_resource_connection = patch(
-            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.resource_module.get_resource_connection"
+            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module_baseget_resource_connection"
         )
         self.get_resource_connection = (
             self.mock_get_resource_connection.start()
