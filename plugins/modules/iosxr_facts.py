93183a2227a87a3fb12c84ab4cd78c6939116714
Rohit Thakur <rohitthakur2590@outlook.com>
2021-04-09 14:40:22 +0530

    available_network_resources implemented in iosxr_facts
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/modules/iosxr_facts.py
+++ plugins/modules/iosxr_facts.py
@@ -52,6 +52,10 @@ options:
     required: false
     type: list
     elements: str
+  available_network_resources:
+    description: When 'True' a list of network resources for which resource modules are available will be provided.
+    type: bool
+    default: false
 """
 
 EXAMPLES = """
@@ -184,6 +188,7 @@ from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.argspec.
 )
 from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.facts import (
     Facts,
+    FACT_RESOURCE_SUBSETS
 )
 
 
@@ -206,9 +211,14 @@ def main():
             "default value for `gather_subset` will be changed to `min` from `!config` v2.11 onwards"
         )
 
+    ansible_facts = {}
+    if module.params.get("available_network_resources"):
+        ansible_facts["available_network_resources"] = sorted(
+            FACT_RESOURCE_SUBSETS.keys()
+        )
     result = Facts(module).get_facts()
-
-    ansible_facts, additional_warnings = result
+    additional_facts, additional_warnings = result
+    ansible_facts.update(additional_facts)
     warnings.extend(additional_warnings)
 
     module.exit_json(ansible_facts=ansible_facts, warnings=warnings)
