7d3525792ab8e57d5c3e1cff75491ad857ef70f2
ashwini-mhatre <mashu97@gmail.com>
2021-08-03 15:08:34 +0530

    add uts for lag-interfaces and acl-interfaces

--- tests/unit/modules/network/iosxr/test_iosxr_lag_interfaces.py
+++ tests/unit/modules/network/iosxr/test_iosxr_lag_interfaces.py
@@ -22,18 +22,18 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from ansible_collections.cisco.iosxr.tests.unit.compat.mock import patch
-from ansible_collections.cisco.iosxr.plugins.modules import iosxr_l3_interfaces
+from ansible_collections.cisco.iosxr.plugins.modules import iosxr_lag_interfaces
 from ansible_collections.cisco.iosxr.tests.unit.modules.utils import (
     set_module_args,
 )
 from .iosxr_module import TestIosxrModule, load_fixture
 
 
-class TestIosxrL3InterfacesModule(TestIosxrModule):
-    module = iosxr_l3_interfaces
+class TestIosxrLagInterfacesModule(TestIosxrModule):
+    module = iosxr_lag_interfaces
 
     def setUp(self):
-        super(TestIosxrL3InterfacesModule, self).setUp()
+        super(TestIosxrLagInterfacesModule, self).setUp()
 
         self.mock_get_config = patch(
             "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.network.Config.get_config"
@@ -59,12 +59,12 @@ class TestIosxrL3InterfacesModule(TestIosxrModule):
             self.mock_get_resource_connection_facts.start()
         )
         self.mock_execute_show_command = patch(
-            "ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.l3_interfaces.l3_interfaces.L3_InterfacesFacts.get_config"
+            "ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.lag_interfaces.lag_interfaces.Lag_interfacesFacts.get_config"
         )
         self.execute_show_command = self.mock_execute_show_command.start()
 
     def tearDown(self):
-        super(TestIosxrL3InterfacesModule, self).tearDown()
+        super(TestIosxrLagInterfacesModule, self).tearDown()
         self.mock_get_resource_connection_config.stop()
         self.mock_get_resource_connection_facts.stop()
         self.mock_get_config.stop()
@@ -73,41 +73,45 @@ class TestIosxrL3InterfacesModule(TestIosxrModule):
 
     def _prepare(self):
         def load_from_file(*args, **kwargs):
-            return load_fixture("iosxr_l3_interface_config.cfg")
+            return load_fixture("iosxr_lag_interface_config.cfg")
 
         self.execute_show_command.side_effect = load_from_file
 
-    def test_iosxr_l3_interfaces_merged_idempotent(self):
+    def test_iosxr_lag_interfaces_merged_idempotent(self):
         self._prepare()
         set_module_args(
             dict(
                 config=[
                     dict(
-                        name="GigabitEthernet0/0/0/0",
-                        ipv4=[
+                        name="Bundle-Ether10",
+                        mode="active",
+                        members=[
                             dict(
-                                address="198.51.100.1/24"
+                                member="GigabitEthernet0/0/0/0",
+                                mode="inherit"
+                            ),
+                            dict(
+                                member="GigabitEthernet0/0/0/1",
+                                mode="passive"
                             )
                         ],
-                        ipv6=[
-                            dict(
-                                address="2001:db8::/32"
-                            )
-                        ]
-
-
+                        links=dict(
+                            max_active=10,
+                            min_active=2
+                        )
                     ),
                     dict(
-                        name="GigabitEthernet0/0/0/1",
-                        ipv4=[
+                        name="Bundle-Ether11",
+                        mode="active",
+                        members=[
                             dict(
-                                address="192.0.2.1/24"
+                                member="GigabitEthernet0/0/0/8",
+                                mode="passive"
                             ),
                             dict(
-                                address="192.0.2.2/24",
-                                secondary=True
-                            ),
-
+                                member="GigabitEthernet0/0/0/9",
+                                mode="passive"
+                            )
                         ]
                     ),
                 ],
@@ -116,35 +120,40 @@ class TestIosxrL3InterfacesModule(TestIosxrModule):
         )
         self.execute_module(changed=False, commands=[])
 
-    def test_iosxr_l3_interfaces_merged(self):
+    def test_iosxr_lag_interfaces_merged(self):
         set_module_args(
             dict(
                 config=[
                     dict(
-                        name="GigabitEthernet0/0/0/0",
-                        ipv4=[
+                        name="Bundle-Ether10",
+                        mode="active",
+                        members=[
                             dict(
-                                address="198.51.100.1/24"
+                                member="GigabitEthernet0/0/0/0",
+                                mode="inherit"
+                            ),
+                            dict(
+                                member="GigabitEthernet0/0/0/1",
+                                mode="passive"
                             )
-                        ]
-
+                        ],
+                        links=dict(
+                            max_active=10,
+                            min_active=2
+                        )
                     ),
                     dict(
-                        name="GigabitEthernet0/0/0/1",
-                        ipv4=[
+                        name="Bundle-Ether11",
+                        mode="active",
+                        members=[
                             dict(
-                                address="192.0.2.1/24"
+                                member="GigabitEthernet0/0/0/8",
+                                mode="passive"
                             ),
                             dict(
-                                address="192.0.2.2/24",
-                                secondary=True
-                            ),
-
-                        ],
-                        ipv6=[
-                            dict(
-                                address="2001:db8:0:3::/64"
-                            ),
+                                member="GigabitEthernet0/0/0/9",
+                                mode="passive"
+                            )
                         ]
                     ),
                 ],
@@ -152,90 +161,119 @@ class TestIosxrL3InterfacesModule(TestIosxrModule):
             )
         )
         commands = [
-            "interface GigabitEthernet0/0/0/0",
-            "ipv4 address 198.51.100.1 255.255.255.0",
+            "interface Bundle-Ether10",
+            "bundle minimum-active links 2",
+            "bundle maximum-active links 10",
+            "lacp mode active",
             "interface GigabitEthernet0/0/0/1",
-            "ipv4 address 192.0.2.2 255.255.255.0 secondary",
-            "ipv4 address 192.0.2.1 255.255.255.0",
-            "ipv6 address 2001:db8:0:3::/64"
+            "bundle id 10 mode passive",
+            "interface GigabitEthernet0/0/0/0",
+            "bundle id 10 mode inherit",
+            "interface Bundle-Ether11",
+            "lacp mode active",
+            "interface GigabitEthernet0/0/0/8",
+            "bundle id 11 mode passive",
+            "interface GigabitEthernet0/0/0/9",
+            "bundle id 11 mode passive"
         ]
         result = self.execute_module(changed=True)
         self.assertEqual(sorted(result["commands"]), sorted(commands))
 
-    def test_iosxr_l3_interfaces_replaced(self):
+    def test_iosxr_lag_interfaces_replaced(self):
         self._prepare()
         set_module_args(
             dict(
                 config=[
                     dict(
-                        name="GigabitEthernet0/0/0/0",
-                        ipv4=[
+                        name="Bundle-Ether10",
+                        mode="passive",
+                        members=[
                             dict(
-                                address="203.0.113.27/24"
-                            ),
-                            dict(
-                                address="203.0.114.1/24",
-                                secondary=True
+                                member="GigabitEthernet0/0/0/0",
+                                mode="passive"
                             )
                         ]
-
+                    ),
+                    dict(
+                        name="Bundle-Ether12",
+                        mode="active"
                     )
                 ],
                 state="replaced",
             )
         )
         commands = [
+            "interface Bundle-Ether10",
+            "no bundle maximum-active links 10",
+            "no bundle minimum-active links 2",
+            "lacp mode passive",
+            "interface GigabitEthernet0/0/0/1",
+            "no bundle id",
             "interface GigabitEthernet0/0/0/0",
-            "no ipv6 address",
-            "ipv4 address 203.0.113.27 255.255.255.0",
-            "ipv4 address 203.0.114.1 255.255.255.0 secondary"
+            "bundle id 10 mode passive",
+            "interface Bundle-Ether12",
+            "lacp mode active"
         ]
         result = self.execute_module(changed=True)
         self.assertEqual(sorted(result["commands"]), sorted(commands))
 
-    def test_iosxr_l3_interfaces_deleted(self):
+    def test_iosxr_lag_interfaces_deleted(self):
         self._prepare()
         set_module_args(dict(state="deleted"))
 
         commands = [
+            "interface Bundle-Ether10",
+            "no bundle maximum-active links 10",
+            "no bundle minimum-active links 2",
+            "no lacp mode active",
             "interface GigabitEthernet0/0/0/0",
-            "no ipv4 address",
-            "no ipv6 address",
+            "no bundle id",
             "interface GigabitEthernet0/0/0/1",
-            "no ipv4 address"
+            "no bundle id",
+            "interface Bundle-Ether11",
+            "no lacp mode active",
+            "interface GigabitEthernet0/0/0/8",
+            "no bundle id",
+            "interface GigabitEthernet0/0/0/9",
+            "no bundle id"
         ]
         result = self.execute_module(changed=True)
         self.assertEqual(sorted(result["commands"]), sorted(commands))
 
-    def test_iosxr_l3_interfaces_rendered(self):
+    def test_iosxr_lag_interfaces_rendered(self):
         set_module_args(
             dict(
                 config=[
                     dict(
-                        name="GigabitEthernet0/0/0/0",
-                        ipv4=[
+                        name="Bundle-Ether10",
+                        mode="active",
+                        members=[
                             dict(
-                                address="198.51.100.1/24"
+                                member="GigabitEthernet0/0/0/0",
+                                mode="passive"
+                            ),
+                            dict(
+                                member="GigabitEthernet0/0/0/1",
+                                mode="passive"
                             )
-                        ]
-
+                        ],
+                        links=dict(
+                            max_active=10,
+                            min_active=2
+                        )
                     ),
                     dict(
-                        name="GigabitEthernet0/0/0/1",
-                        ipv4=[
+                        name="Bundle-Ether11",
+                        mode="active",
+                        members=[
                             dict(
-                                address="192.0.2.1/24"
+                                member="GigabitEthernet0/0/0/8",
+                                mode="passive"
                             ),
                             dict(
-                                address="192.0.2.2/24",
-                                secondary=True
-                            ),
-
-                        ],
-                        ipv6=[
-                            dict(
-                                address="2001:db8:0:3::/64"
-                            ),
+                                member="GigabitEthernet0/0/0/9",
+                                mode="passive"
+                            )
                         ]
                     ),
                 ],
@@ -244,64 +282,97 @@ class TestIosxrL3InterfacesModule(TestIosxrModule):
         )
 
         commands = [
-            "interface GigabitEthernet0/0/0/0",
-            "ipv4 address 198.51.100.1 255.255.255.0",
+            "interface Bundle-Ether10",
+            "bundle minimum-active links 2",
+            "bundle maximum-active links 10",
+            "lacp mode active",
             "interface GigabitEthernet0/0/0/1",
-            "ipv4 address 192.0.2.2 255.255.255.0 secondary",
-            "ipv4 address 192.0.2.1 255.255.255.0",
-            "ipv6 address 2001:db8:0:3::/64"
+            "bundle id 10 mode passive",
+            "interface GigabitEthernet0/0/0/0",
+            "bundle id 10 mode passive",
+            "interface Bundle-Ether11",
+            "lacp mode active",
+            "interface GigabitEthernet0/0/0/8",
+            "bundle id 11 mode passive",
+            "interface GigabitEthernet0/0/0/9",
+            "bundle id 11 mode passive"
         ]
         result = self.execute_module(changed=False)
         self.assertEqual(sorted(result["rendered"]), sorted(commands))
 
-    def test_iosxr_l3_interfaces_parsed(self):
+    def test_iosxr_lag_interfaces_parsed(self):
         self.maxDiff = None
         set_module_args(
             dict(
-                running_config="interface GigabitEthernet0/0/0/0\nipv4 address 198.51.100.1 255.255.255.0\n"
-                               "ipv6 address 2001:db8::/32\ninterface GigabitEthernet0/0/0/1\nipv4 address"
-                               " 192.0.2.1 255.255.255.0\nipv4 address 192.0.2.2 255.255.255.0 secondary\n",
+                running_config="interface Bundle-Ether10\r\n lacp mode active\r\n bundle maximum-active "
+                               "links 10\r\n bundle minimum-active links 2\r\n!\r\ninterface Bundle-Ether11"
+                               "\r\nlacp mode active\r\n!\r\ninterface GigabitEthernet0/0/0/0\r\n description "
+                               "\"GigabitEthernet - 0\"\r\n bundle id 10 mode inherit\r\n!\r\ninterface "
+                               "GigabitEthernet0/0/0/1"
+                               "\r\n description \"GigabitEthernet - 2\"\r\n  bundle id 10 mode passive\r\n!\r\n"
+                               "interface GigabitEthernet0/0/0/8\r\n description \"GigabitEthernet - 8\""
+                               "\r\n bundle id 11 mode passive"
+                               "\r\n!\r\ninterface GigabitEthernet0/0/0/9\r\n description "
+                               "\"GigabitEthernet - 9\"\r\n bundle id 11 mode passive\r\n!",
                 state="parsed",
             )
         )
         result = self.execute_module(changed=False)
         print(result["parsed"])
-        parsed_list = [{'name': 'GigabitEthernet0/0/0/0', 'ipv4': [{'address': '198.51.100.1 255.255.255.0'}], 'ipv6':
-                        [{'address': '2001:db8::/32'}]}, {'name': 'GigabitEthernet0/0/0/1', 'ipv4': [
-                            {'address': '192.0.2.1 255.255.255.0'}, {'address': '192.0.2.2 255.255.255.0',
-                                                                     'secondary': True}]}]
+        parsed_list = [{'links': {'max_active': 10, 'min_active': 2}, 'members': [
+            {'member': 'GigabitEthernet0/0/0/0', 'mode': 'inherit'}, {'member': 'GigabitEthernet0/0/0/1',
+            'mode': 'passive'}], 'mode': 'active', 'name':  'Bundle-Ether10'}, {'members': [
+            {'member': 'GigabitEthernet0/0/0/8', 'mode': 'passive'},
+             {'member': 'GigabitEthernet0/0/0/9', 'mode': 'passive'}],
+            'mode': 'active', 'name': 'Bundle-Ether11'}]
         self.assertEqual(parsed_list, result["parsed"])
 
-    def test_iosxr_l3_interfaces_overridden(self):
+    def test_iosxr_lag_interfaces_overridden(self):
         self.maxDiff = None
         self._prepare()
         set_module_args(dict(
             config=[
                 dict(
-                    name="GigabitEthernet0/0/0/1",
-                    ipv4=[
+                    name="Bundle-Ether11",
+                    mode="active",
+                    members=[
                         dict(
-                            address="198.51.102.1/24"
-                        )
-
-                    ],
-                    ipv6=[
-                        dict(
-                            address="2001:db8:1::/64"
+                            member="GigabitEthernet0/0/0/0",
+                            mode="active"
                         ),
-                    ]
-                ),
+                        dict(
+                            member="GigabitEthernet0/0/0/1",
+                            mode="active"
+                        )
+                    ],
+                    links=dict(
+                        max_active=10,
+                        min_active=5
+                    )
+                )
             ],
             state="overridden")
         )
         commands = [
+            "interface Bundle-Ether10",
+            "no bundle maximum-active links 10",
+            "no bundle minimum-active links 2",
+            "no lacp mode active",
             "interface GigabitEthernet0/0/0/0",
-            "no ipv4 address",
-            "no ipv6 address",
+            "no bundle id",
             "interface GigabitEthernet0/0/0/1",
-            "no ipv4 address",
-            "ipv4 address 198.51.102.1 255.255.255.0",
-            "ipv6 address 2001:db8:1::/64"
+            "no bundle id",
+            "interface Bundle-Ether11",
+            "bundle minimum-active links 5",
+            "bundle maximum-active links 10",
+            "interface GigabitEthernet0/0/0/8",
+            "no bundle id",
+            "interface GigabitEthernet0/0/0/9",
+            "no bundle id",
+            "interface GigabitEthernet0/0/0/0",
+            "bundle id 11 mode active",
+            "interface GigabitEthernet0/0/0/1",
+            "bundle id 11 mode active"
         ]
 
         result = self.execute_module(changed=True)
