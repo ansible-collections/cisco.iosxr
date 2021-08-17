08b26e5ee1edacffd42d48d4f07f6c8dd1b368dd
ashwini-mhatre <mashu97@gmail.com>
2021-08-17 21:05:07 +0530

    fix sanity

--- plugins/module_utils/network/iosxr/config/l2_interfaces/l2_interfaces.py
+++ plugins/module_utils/network/iosxr/config/l2_interfaces/l2_interfaces.py
@@ -334,15 +334,15 @@ class L2_Interfaces(ConfigBase):
                 if l2transport or l2protocol:
                     for each in l2protocol:
                         each = dict(each)
-                        if (
-                            isinstance(each, dict)
-                            and "cpsv" in list(each.keys())
+                        if isinstance(each, dict) and "cpsv" in list(
+                            each.keys()
                         ):
                             cmd = "l2transport l2protocol {0} {1}".format(
                                 "cpsv", each.get("cpsv")
                             )
-                            add_command_to_config_list(interface, cmd, commands)
-                            print (commands)
+                            add_command_to_config_list(
+                                interface, cmd, commands
+                            )
                             break
                 if encapsulation:
                     encapsulation = dict(encapsulation)
