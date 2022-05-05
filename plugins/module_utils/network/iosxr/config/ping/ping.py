db9ec84b7ddf423814fd6765bb9ea944cbeea38f
KB-perByte <sagpaul@redhat.com>
2022-05-05 12:44:39 +0530

    update supp files

---
+++ plugins/module_utils/network/iosxr/config/ping/ping.py
@@ -0,0 +1,154 @@
+#
+# -*- coding: utf-8 -*-
+# Copyright 2022 Red Hat
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+#
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+"""
+The iosxr_ping config file.
+It is in this file where the current configuration (as dict)
+is compared to the provided configuration (as dict) and the command set
+necessary to bring the current configuration to its desired end-state is
+created.
+"""
+# from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
+#     dict_merge,
+# )
+
+# from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module import (
+#     ResourceModule,
+# )
+# from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.facts import (
+#     Facts,
+# )
+# from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.rm_templates.ping import (
+#     PingTemplate,
+# )
+from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.iosxr import (
+    run_commands,
+)
+import re
+
+
+class Ping:  # ResourceModule
+    """
+    The iosxr_ping config class
+    """
+
+    def __init__(self, module):
+        self.module = module
+        self.result = {}
+        # super(Ping, self).__init__(
+        #     empty_fact_val={},
+        #     facts_module=Facts(module),
+        #     module=module,
+        #     resource="ping",
+        #     tmplt=PingTemplate(),
+        # )
+        # self.parsers = []
+
+    def execute_module(self):
+        """Execute the module
+
+        :rtype: A dictionary
+        :returns: The result from module execution
+        """
+        # if self.state not in ["parsed", "gathered"]:
+        self.generate_command()
+        res = self.run_command()
+        return self.process_result(res)
+
+    def build_ping(self, params):
+        # ping vrf paul 10.0.2.15 count 2 df-bit sweep validate size 40 source nve 2
+        # {vrf}{ping_type}{dest}{count}{df_bit}{sweep}{validate}{size}{source}
+
+        cmd = "ping"
+        if params.get("vrf"):
+            cmd += " vrf " + params.get("vrf")
+        if params.get("ping_type"):
+            cmd += " " + params.get("ping_type")
+        if params.get("dest"):
+            cmd += " " + params.get("dest")
+        if params.get("count"):
+            cmd += " count " + str(params.get("count"))
+        if params.get("df_bit"):
+            cmd += " df-bit"
+        if params.get("sweep"):
+            cmd += " sweep"
+        if params.get("validate"):
+            cmd += " validate"
+        if params.get("size"):
+            cmd += " size " + str(params.get("size"))
+        if params.get("source"):
+            cmd += " source " + params.get("source")
+        return cmd
+
+    def parse_ping(self, ping_stats):
+        """
+        Function used to parse the statistical information from the ping response.
+        Example: "Success rate is 100 percent (5/5), round-trip min/avg/max = 1/2/8 ms"
+        Returns the percent of packet loss, received packets, transmitted packets, and RTT dict.
+        """
+        rate_re = re.compile(
+            "^\\w+\\s+\\w+\\s+\\w+\\s+(?P<pct>\\d+)\\s+\\w+\\s+\\((?P<rx>\\d+)/(?P<tx>\\d+)\\)"
+        )
+        rtt_re = re.compile(
+            ".*,\\s+\\S+\\s+\\S+\\s+=\\s+(?P<min>\\d+)/(?P<avg>\\d+)/(?P<max>\\d+)\\s+\\w+\\s*$|.*\\s*$"
+        )
+        rate = rate_re.match(ping_stats)
+        rtt = rtt_re.match(ping_stats)
+        return (
+            rate.group("pct"),
+            rate.group("rx"),
+            rate.group("tx"),
+            rtt.groupdict(),
+        )
+
+    def validate_results(self, module, loss, results):
+        """
+        This function is used to validate whether the ping results were unexpected per "state" param.
+        """
+        state = module.params["state"]
+        if state == "present" and loss == 100:
+            module.fail_json(msg="Ping failed unexpectedly", **results)
+        elif state == "absent" and loss < 100:
+            module.fail_json(msg="Ping succeeded unexpectedly", **results)
+
+    def generate_command(self):
+        """Generate configuration commands to send based on
+        want, have and desired state.
+        """
+        warnings = list()  # no idea why
+        if warnings:
+            self.result["warnings"] = warnings
+        self.result["commands"] = self.build_ping(self.module.params)
+
+    def run_command(self):
+        ping_results = run_commands(
+            self.module, commands=self.result["commands"]
+        )
+        return ping_results
+
+    def process_result(self, ping_results):
+        ping_results_list = ping_results[0].split("\n")
+        stats = ""
+        for line in ping_results_list:
+            if line.startswith("Success"):
+                stats = line
+        success, rx, tx, rtt = self.parse_ping(stats)
+        loss = abs(100 - int(success))
+        self.result["packet_loss"] = str(loss) + "%"
+        self.result["packets_rx"] = int(rx)
+        self.result["packets_tx"] = int(tx)
+        # Convert rtt values to int
+        for k, v in rtt.items():
+            if rtt[k] is not None:
+                rtt[k] = int(v)
+        self.result["rtt"] = rtt
+        self.validate_results(self.module, loss, self.result)
+        return self.result
--- plugins/modules/iosxr_ping.py
+++ plugins/modules/iosxr_ping.py
@@ -11,6 +11,7 @@ The module file for iosxr_ping
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
+
 DOCUMENTATION = """
 module: iosxr_ping
 short_description: Tests reachability using ping from IOSXR switch.
@@ -86,6 +87,7 @@ notes:
   module instead.
 - For targets running Python, use the L(ping,https://docs.ansible.com/ansible/latest/collections/ansible/builtin/ping_module.html) module instead.
 """
+
 EXAMPLES = """
 - name: Test reachability to 10.10.10.10 using default vrf
   cisco.iosxr.iosxr_ping:
@@ -110,133 +112,77 @@ EXAMPLES = """
     df_bit: true
     size: 1400
 """
+
 RETURN = """
-commands:
-  description: Show the command sent.
-  returned: always
-  type: list
-  sample: ["ping vrf prod 10.40.40.40 count 20 source loopback0"]
-packet_loss:
-  description: Percentage of packets lost.
-  returned: always
-  type: str
-  sample: "0%"
-packets_rx:
-  description: Packets successfully received.
-  returned: always
-  type: int
-  sample: 20
-packets_tx:
-  description: Packets successfully transmitted.
-  returned: always
-  type: int
-  sample: 20
-rtt:
-  description: Show RTT stats.
-  returned: always
+before:
+  description: The configuration prior to the module execution.
+  returned: when I(state) is C(merged), C(replaced), C(overridden), C(deleted) or C(purged)
   type: dict
-  sample: {"avg": 2, "max": 8, "min": 1}
+  sample: >
+    This output will always be in the same format as the
+    module argspec.
+after:
+  description: The resulting configuration after module execution.
+  returned: when changed
+  type: dict
+  sample: >
+    This output will always be in the same format as the
+    module argspec.
+commands:
+  description: The set of commands pushed to the remote device.
+  returned: when I(state) is C(merged), C(replaced), C(overridden), C(deleted) or C(purged)
+  type: list
+  sample:
+    - sample command 1
+    - sample command 2
+    - sample command 3
+rendered:
+  description: The provided configuration in the task rendered in device-native format (offline).
+  returned: when I(state) is C(rendered)
+  type: list
+  sample:
+    - sample command 1
+    - sample command 2
+    - sample command 3
+gathered:
+  description: Facts about the network resource gathered from the remote device as structured data.
+  returned: when I(state) is C(gathered)
+  type: list
+  sample: >
+    This output will always be in the same format as the
+    module argspec.
+parsed:
+  description: The device native config provided in I(running_config) option parsed into structured data as per module argspec.
+  returned: when I(state) is C(parsed)
+  type: list
+  sample: >
+    This output will always be in the same format as the
+    module argspec.
 """
+
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.iosxr import (
-    run_commands,
+from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.argspec.ping.ping import (
+    PingArgs,
 )
-from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.iosxr import (
-    iosxr_argument_spec,
+from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.config.ping.ping import (
+    Ping,
 )
-import re
+import debugpy
+
+debugpy.listen(3000)
+debugpy.wait_for_client()
 
 
 def main():
-    """main entry point for module execution"""
-    argument_spec = dict(
-        count=dict(type="int"),
-        dest=dict(type="str", required=True),
-        df_bit=dict(type="bool", default=False),
-        size=dict(type="int"),
-        source=dict(type="str"),
-        state=dict(type="str", choices=["absent", "present"], default="present"),
-        vrf=dict(type="str"),
-    )
-    argument_spec.update(iosxr_argument_spec)
-    module = AnsibleModule(argument_spec=argument_spec)
-    count = module.params["count"]
-    dest = module.params["dest"]
-    df_bit = module.params["df_bit"]
-    size = module.params["size"]
-    source = module.params["source"]
-    vrf = module.params["vrf"]
-    warnings = list()
-    results = {}
-    if warnings:
-        results["warnings"] = warnings
-    results["commands"] = [build_ping(dest, count, source, vrf, size, df_bit)]
-    ping_results = run_commands(module, commands=results["commands"])
-    ping_results_list = ping_results[0].split("\n")
-    stats = ""
-    for line in ping_results_list:
-        if line.startswith("Success"):
-            stats = line
-    success, rx, tx, rtt = parse_ping(stats)
-    loss = abs(100 - int(success))
-    results["packet_loss"] = str(loss) + "%"
-    results["packets_rx"] = int(rx)
-    results["packets_tx"] = int(tx)
-    # Convert rtt values to int
-    for k, v in rtt.items():
-        if rtt[k] is not None:
-            rtt[k] = int(v)
-    results["rtt"] = rtt
-    validate_results(module, loss, results)
-    module.exit_json(**results)
-
-
-def build_ping(dest, count=None, source=None, vrf=None, size=None, df_bit=False):
     """
-    Function to build the command to send to the terminal for the switch
-    to execute. All args come from the module's unique params.
-    """
-    if vrf is not None:
-        cmd = "ping vrf {0} {1}".format(vrf, dest)
-    else:
-        cmd = "ping {0}".format(dest)
-    if count is not None:
-        cmd += " repeat {0}".format(str(count))
-    if size is not None:
-        cmd += " size {0}".format(size)
-    if df_bit:
-        cmd += " df-bit"
-    if source is not None:
-        cmd += " source {0}".format(source)
-    return cmd
+    Main entry point for module execution
 
+    :returns: the result form module invocation
+    """
+    module = AnsibleModule(argument_spec=PingArgs.argument_spec)
 
-def parse_ping(ping_stats):
-    """
-    Function used to parse the statistical information from the ping response.
-    Example: "Success rate is 100 percent (5/5), round-trip min/avg/max = 1/2/8 ms"
-    Returns the percent of packet loss, received packets, transmitted packets, and RTT dict.
-    """
-    rate_re = re.compile(
-        "^\\w+\\s+\\w+\\s+\\w+\\s+(?P<pct>\\d+)\\s+\\w+\\s+\\((?P<rx>\\d+)/(?P<tx>\\d+)\\)"
-    )
-    rtt_re = re.compile(
-        ".*,\\s+\\S+\\s+\\S+\\s+=\\s+(?P<min>\\d+)/(?P<avg>\\d+)/(?P<max>\\d+)\\s+\\w+\\s*$|.*\\s*$"
-    )
-    rate = rate_re.match(ping_stats)
-    rtt = rtt_re.match(ping_stats)
-    return (rate.group("pct"), rate.group("rx"), rate.group("tx"), rtt.groupdict())
-
-
-def validate_results(module, loss, results):
-    """
-    This function is used to validate whether the ping results were unexpected per "state" param.
-    """
-    state = module.params["state"]
-    if state == "present" and loss == 100:
-        module.fail_json(msg="Ping failed unexpectedly", **results)
-    elif state == "absent" and loss < 100:
-        module.fail_json(msg="Ping succeeded unexpectedly", **results)
+    result = Ping(module).execute_module()
+    module.exit_json(**result)
 
 
 if __name__ == "__main__":
