1037065ede081cfad8a1a598c414329aaaa5cf8a
ashwini-mhatre <mashu97@gmail.com>
2023-01-25 22:55:00 +0530

    Fix sanity

--- plugins/module_utils/network/iosxr/argspec/bgp_global/bgp_global.py
+++ plugins/module_utils/network/iosxr/argspec/bgp_global/bgp_global.py
@@ -863,6 +863,7 @@ class Bgp_globalArgs(object):  # pylint: disable=R0903
                                         "fast_detect": {
                                             "type": "dict",
                                             "options": {
+                                                "set": {"type": "bool"},
                                                 "disable": {"type": "bool"},
                                                 "strict_mode": {
                                                     "type": "bool",
