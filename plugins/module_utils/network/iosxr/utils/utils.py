052656d83a1769f639625fa9be3d05ae24b81c13
Ashwini Mhatre <mashu97@gmail.com>
2023-02-23 15:25:00 +0530

    Fix imports (#342)
    * Fix imports
    
    * Fix imports

--- plugins/module_utils/network/iosxr/utils/utils.py
+++ plugins/module_utils/network/iosxr/utils/utils.py
@@ -14,12 +14,11 @@ from functools import total_ordering
 
 from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import missing_required_lib
+from ansible.module_utils.common.network import is_masklen, to_netmask
 from ansible.module_utils.six import iteritems
 from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
     dict_diff,
-    is_masklen,
     search_obj_in_list,
-    to_netmask,
 )
 
 
