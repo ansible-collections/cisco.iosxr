ca0735ffb400ee125eae0e3e7b33a7565d98a919
ashwini-mhatre <mashu97@gmail.com>
2023-03-20 18:49:53 +0530

    Add integration tests

--- tests/unit/modules/network/iosxr/test_iosxr_bgp_templates.py
+++ tests/unit/modules/network/iosxr/test_iosxr_bgp_templates.py
@@ -535,8 +535,7 @@ class TestIosxrBgptemplatesModule(TestIosxrModule):
                                     route_reflector_client=dict(set=True),
                                     allowas_in=dict(value=1),
                                     maximum_prefix=dict(
-                                        max_limit=1,
-                                        threshold_value=75,
+                                        warning_only=True,
                                     ),
                                     as_override=dict(set=True),
                                     capability_orf_prefix="send",
@@ -610,7 +609,7 @@ class TestIosxrBgptemplatesModule(TestIosxrModule):
             "capability orf prefix send",
             "default-originate",
             "long-lived-graceful-restart capable",
-            "maximum-prefix 1 75",
+            "maximum-prefix warning-only",
             "multipath",
             "next-hop-self",
             "next-hop-unchanged multipath",
@@ -944,7 +943,6 @@ class TestIosxrBgptemplatesModule(TestIosxrModule):
             ),
         )
         self.get_config.return_value = run_cfg
-
         commands = [
             "router bgp 1",
             "no neighbor-group test",
@@ -997,3 +995,240 @@ class TestIosxrBgptemplatesModule(TestIosxrModule):
         ]
         result = self.execute_module(changed=True)
         self.assertEqual(sorted(result["commands"]), sorted(commands))
+
+    def test_iosxr_bgp_tmpl_parsed(self):
+        self.maxDiff = None
+        set_module_args(
+            dict(
+                running_config="router bgp 1\n neighbor-group ********\n  bfd fast-detect strict-mode\n  update in filtering\n   message buffers 0 non-circular\n   logging disable\n  !\n  precedence critical\n  timers 0 3 3\n  advertisement-interval 10\n  maximum-peers 10\n  local-address-subnet 1.1.1.5/24\n  internal-vpn-client\n  address-family ipv4 unicast\n   advertise local-labeled-route\n  !\n !\n neighbor-group ********1\n  bfd fast-detect\n  update in filtering\n  !\n  bfd minimum-interval 3\n  capability additional-paths send disable\n  keychain ********\n  ebgp-multihop 255\n  egress-engineering\n  precedence flash\n  graceful-maintenance\n   as-prepends 0\n  !\n  advertisement-interval 2\n  tcp mss inheritance-disable\n  local-as 6\n  password inheritance-disable\n  shutdown inheritance-disable\n  maximum-peers 1\n  cluster-id 1\n  dmz-link-bandwidth\n  description ********\n  ttl-security\n  ebgp-recv-extcommunity-dmz\n  ebgp-send-extcommunity-dmz cumulative\n  local address inheritance-disable\n  update-source Loopback919\n  idle-watch-time 30\n  enforce-first-as\n  ignore-connected-check\n  session-open-mode both\n  send-buffer-size 4096\n  local-address-subnet 1.1.1.1/24\n  receive-buffer-size 512\n  internal-vpn-client\n  address-family ipv4 unicast\n   origin-as validation disable\n   bestpath origin-as allow invalid\n   weight 0\n   aigp send med\n   send-community-ebgp\n   multipath\n   route-reflector-client\n   allowas-in 1\n   encapsulation-type srv6\n   maximum-prefix 1 75\n   as-override\n   capability orf prefix send\n   send-extended-community-ebgp\n   default-originate\n   long-lived-graceful-restart capable\n   next-hop-self\n   send-community-gshut-ebgp inheritance-disable\n   soft-reconfiguration inbound\n   site-of-origin 1.1:1\n   send-multicast-attributes disable\n   Signalling bgp disable\n   remove-private-AS inbound\n   update out originator-loopcheck disable\n   advertise local-labeled-route\n   next-hop-unchanged inheritance-disable\n   cluster-id allow-equal disable\n  !       \n !        \n neighbor 1.1.1.1\n !        \n!",
+                state="parsed",
+            ),
+        )
+        result = self.execute_module(changed=False)
+        parsed_list = {
+            "as_number": "1",
+            "neighbor": [
+                {
+                    "name": "********",
+                    "bfd": {"fast_detect": {"strict_mode": True}},
+                    "update": {"in": {"filtering": {"logging": {"disable": True}}}},
+                    "precedence": "critical",
+                    "advertisement_interval": 10,
+                    "maximum_peers": 10,
+                    "internal_vpn_client": True,
+                    "address_family": [
+                        {
+                            "afi": "ipv4",
+                            "safi": "unicast",
+                            "advertise": {"local_labeled_route": {"set": True}},
+                        },
+                    ],
+                },
+                {
+                    "name": "********1",
+                    "bfd": {"fast_detect": {"set": True}, "minimum_interval": 3},
+                    "capability": {"additional_paths": {"send": {"disable": True}}},
+                    "keychain": {"name": "********"},
+                    "ebgp_multihop": {"value": 255},
+                    "egress_engineering": {"set": True},
+                    "precedence": "flash",
+                    "graceful_maintenance": {"set": True, "as_prepends": {"value": 0}},
+                    "advertisement_interval": 2,
+                    "tcp": {"mss": {"inheritance_disable": True}},
+                    "local_as": {"value": 6},
+                    "password": {"inheritance_disable": True},
+                    "maximum_peers": 1,
+                    "cluster_id": "1",
+                    "dmz_link_bandwidth": {"set": True},
+                    "description": "********",
+                    "ttl_security": {"set": True},
+                    "ebgp_recv_extcommunity_dmz": {"set": True},
+                    "local": {"address": {"inheritance_disable": True}},
+                    "update_source": "Loopback919",
+                    "idle_watch_time": 30,
+                    "ignore_connected_check": {"set": True},
+                    "session_open_mode": "both",
+                    "send_buffer_size": 4096,
+                    "receive_buffer_size": 512,
+                    "internal_vpn_client": True,
+                    "address_family": [
+                        {
+                            "afi": "ipv4",
+                            "safi": "unicast",
+                            "origin_as": {"validation": {"disable": True}},
+                            "bestpath_origin_as_allow_invalid": True,
+                            "weight": 0,
+                            "aigp": {"send_med": {"set": True}},
+                            "send_community_ebgp": {"set": True},
+                            "multipath": True,
+                            "route_reflector_client": {"set": True},
+                            "allowas_in": {"value": 1},
+                            "encapsulation_type_srv6": True,
+                            "maximum_prefix": {"max_limit": 1, "threshold_value": 75},
+                            "as_override": {"set": True},
+                            "capability_orf_prefix": "send",
+                            "send_extended_community_ebgp": {"set": True},
+                            "default_originate": {"set": True},
+                            "long_lived_graceful_restart": {"capable": True},
+                            "next_hop_self": {"set": True},
+                            "send_community_gshut_ebgp": {"inheritance_disable": True},
+                            "soft_reconfiguration": {"inbound": {"set": True}},
+                            "send_multicast_attributes": {"disable": True},
+                            "signalling": {"bgp_disable": True},
+                            "remove_private_AS": {"inbound": True},
+                            "update": {"out_originator_loopcheck_disable": True},
+                            "advertise": {"local_labeled_route": {"set": True}},
+                            "next_hop_unchanged": {"inheritance_disable": True},
+                        },
+                    ],
+                },
+            ],
+        }
+        self.assertEqual(parsed_list, result["parsed"])
+
+    def test_iosxr_bgp_tmpl_gathered(self):
+        self.maxDiff = None
+        run_cfg = dedent(
+            """\
+                router bgp 1
+                 neighbor-group test
+                  bfd fast-detect strict-mode
+                  precedence critical
+                  advertisement-interval 10
+                  internal-vpn-client
+                  address-family ipv4 unicast
+                   advertise local-labeled-route
+                  !
+                 !
+                 neighbor-group test1
+                  bfd fast-detect
+                  bfd minimum-interval 3
+                  keychain test
+                  ebgp-multihop 255
+                  egress-engineering
+                  precedence flash
+                  graceful-maintenance
+                   as-prepends 0
+                  !
+                  advertisement-interval 2
+                  tcp mss inheritance-disable
+                  local-as 6
+                  password inheritance-disable
+                  cluster-id 1
+                  dmz-link-bandwidth
+                  description test
+                  ttl-security
+                  local address inheritance-disable
+                  update-source Loopback919
+                  idle-watch-time 30
+                  ignore-connected-check
+                  session-open-mode both
+                  send-buffer-size 4096
+                  receive-buffer-size 512
+                  internal-vpn-client
+                  address-family ipv4 unicast
+                   origin-as validation disable
+                   bestpath origin-as allow invalid
+                   aigp
+                   weight 0
+                   send-community-ebgp
+                   multipath
+                   route-reflector-client
+                   allowas-in 1
+                   maximum-prefix 1 75
+                   as-override
+                   capability orf prefix send
+                   send-extended-community-ebgp
+                   default-originate
+                   long-lived-graceful-restart capable
+                   next-hop-self
+                   send-community-gshut-ebgp inheritance-disable
+                   soft-reconfiguration inbound
+                   send-multicast-attributes disable
+                   Signalling bgp disable
+                   remove-private-AS inbound
+                   update out originator-loopcheck disable
+                   advertise local-labeled-route
+                   next-hop-unchanged inheritance-disable
+                  !
+                 !
+
+            """,
+        )
+        self.get_config.return_value = run_cfg
+        set_module_args(dict(state="gathered"))
+        gathered = {
+            "as_number": "1",
+            "neighbor": [
+                {
+                    "name": "test",
+                    "bfd": {"fast_detect": {"strict_mode": True}},
+                    "precedence": "critical",
+                    "advertisement_interval": 10,
+                    "internal_vpn_client": True,
+                    "address_family": [
+                        {
+                            "afi": "ipv4",
+                            "safi": "unicast",
+                            "advertise": {"local_labeled_route": {"set": True}},
+                        },
+                    ],
+                },
+                {
+                    "name": "test1",
+                    "bfd": {"fast_detect": {"set": True}, "minimum_interval": 3},
+                    "keychain": {"name": "test"},
+                    "ebgp_multihop": {"value": 255},
+                    "egress_engineering": {"set": True},
+                    "precedence": "flash",
+                    "graceful_maintenance": {"set": True, "as_prepends": {"value": 0}},
+                    "advertisement_interval": 2,
+                    "tcp": {"mss": {"inheritance_disable": True}},
+                    "local_as": {"value": 6},
+                    "password": {"inheritance_disable": True},
+                    "cluster_id": "1",
+                    "dmz_link_bandwidth": {"set": True},
+                    "description": "test",
+                    "ttl_security": {"set": True},
+                    "local": {"address": {"inheritance_disable": True}},
+                    "update_source": "Loopback919",
+                    "idle_watch_time": 30,
+                    "ignore_connected_check": {"set": True},
+                    "session_open_mode": "both",
+                    "send_buffer_size": 4096,
+                    "receive_buffer_size": 512,
+                    "internal_vpn_client": True,
+                    "address_family": [
+                        {
+                            "afi": "ipv4",
+                            "safi": "unicast",
+                            "origin_as": {"validation": {"disable": True}},
+                            "bestpath_origin_as_allow_invalid": True,
+                            "aigp": {"set": True},
+                            "weight": 0,
+                            "send_community_ebgp": {"set": True},
+                            "multipath": True,
+                            "route_reflector_client": {"set": True},
+                            "allowas_in": {"value": 1},
+                            "maximum_prefix": {"max_limit": 1, "threshold_value": 75},
+                            "as_override": {"set": True},
+                            "capability_orf_prefix": "send",
+                            "send_extended_community_ebgp": {"set": True},
+                            "default_originate": {"set": True},
+                            "long_lived_graceful_restart": {"capable": True},
+                            "next_hop_self": {"set": True},
+                            "send_community_gshut_ebgp": {"inheritance_disable": True},
+                            "soft_reconfiguration": {"inbound": {"set": True}},
+                            "send_multicast_attributes": {"disable": True},
+                            "signalling": {"bgp_disable": True},
+                            "remove_private_AS": {"inbound": True},
+                            "update": {"out_originator_loopcheck_disable": True},
+                            "advertise": {"local_labeled_route": {"set": True}},
+                            "next_hop_unchanged": {"inheritance_disable": True},
+                        },
+                    ],
+                },
+            ],
+        }
+        result = self.execute_module(changed=False)
+        self.assertEqual(gathered, result["gathered"])
