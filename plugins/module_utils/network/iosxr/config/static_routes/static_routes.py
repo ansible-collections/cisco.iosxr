504fab84a50e36530bc73fd21bacf226b2088981
ashwini-mhatre <mashu97@gmail.com>
2023-06-14 13:46:26 +0530

    Fix static routes

--- plugins/module_utils/network/iosxr/config/static_routes/static_routes.py
+++ plugins/module_utils/network/iosxr/config/static_routes/static_routes.py
@@ -155,6 +155,9 @@ class Static_routes(ConfigBase):
                     return "no router static"
 
             else:
+                import epdb
+
+                epdb.serve()
                 for w_item in want:
                     obj_in_have = self._find_vrf(w_item, have)
                     if obj_in_have:
@@ -439,6 +442,49 @@ class Static_routes(ConfigBase):
 
         return commands
 
+    def _static_route_poper(self, want_afi, have_afi):
+        """ """
+        commands = []
+
+        update_commands = []
+        for have_route in have_afi.get("routes", []):
+            want_route = (
+                search_obj_in_list(
+                    have_route["dest"],
+                    want_afi.get("routes", []),
+                    key="dest",
+                )
+                or {}
+            )
+
+            rotated_want_next_hops = self.rotate_next_hops(
+                want_route.get("next_hops", {}),
+            )
+            rotated_have_next_hops = self.rotate_next_hops(
+                have_route.get("next_hops", {}),
+            )
+
+            for key in rotated_want_next_hops.keys():
+                if key not in rotated_have_next_hops:
+                    cmd = "no {0}".format(want_route["dest"])
+                    for item in key:
+                        if "." in item or ":" in item or "/" in item:
+                            cmd += " {0}".format(item)
+                        else:
+                            cmd += " vrf {0}".format(item)
+                    update_commands.append(cmd)
+        if update_commands:
+            update_commands.insert(
+                0,
+                "address-family {0} {1}".format(
+                    have_afi["afi"],
+                    have_afi["safi"],
+                ),
+            )
+            commands.extend(update_commands)
+
+        return commands
+
     def _state_deleted(self, want, have):
         """The command generator when state is deleted
 
@@ -460,12 +506,8 @@ class Static_routes(ConfigBase):
                     or {}
                 )
                 if have_afi:
-                    commands.append(
-                        "no address-family {0} {1}".format(
-                            have_afi["afi"],
-                            have_afi["safi"],
-                        ),
-                    )
+                    commands.extend(self._static_route_poper(want_afi, have_afi))
+
             if "vrf" in want and commands:
                 commands.insert(0, "vrf {0}".format(want["vrf"]))
 
