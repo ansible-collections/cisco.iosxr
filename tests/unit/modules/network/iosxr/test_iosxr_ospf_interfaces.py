2823a95c7b4bdcdb16c370db0f0cad0c316ac3d3
ashwini-mhatre <mashu97@gmail.com>
2023-06-26 17:05:28 +0530

    Adress review comments

--- tests/unit/modules/network/iosxr/test_iosxr_ospf_interfaces.py
+++ tests/unit/modules/network/iosxr/test_iosxr_ospf_interfaces.py
@@ -43,11 +43,6 @@ class TestIosxrOspf_InterfacesModule(TestIosxrModule):
         )
         self.get_resource_connection_facts = self.mock_get_resource_connection_facts.start()
 
-        self.mock_edit_config = patch(
-            "ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.providers.providers.CliProvider.edit_config",
-        )
-        self.edit_config = self.mock_edit_config.start()
-
         self.mock_execute_show_command = patch(
             "ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.ospf_interfaces.ospf_interfaces."
             "Ospf_interfacesFacts.get_ospf_interfaces",
@@ -58,7 +53,6 @@ class TestIosxrOspf_InterfacesModule(TestIosxrModule):
         super(TestIosxrOspf_InterfacesModule, self).tearDown()
         self.mock_get_resource_connection_config.stop()
         self.mock_get_resource_connection_facts.stop()
-        self.mock_edit_config.stop()
         self.mock_get_config.stop()
         self.mock_load_config.stop()
         self.mock_execute_show_command.stop()
