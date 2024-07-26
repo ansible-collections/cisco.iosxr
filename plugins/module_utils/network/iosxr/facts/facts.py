a7ecb28839d22289be89c3c500dfe4cbb84952e0
Ruchi Pakhle <72685035+Ruchip16@users.noreply.github.com>
2024-07-27 00:44:54 +0530

    Add `vrf_address_family` module (#489)
    * Add vrf module [WIP]
    
    * adds parsed state & gathered state
    
    * minor changes in config
    
    * working state - parsed & merged
    
    * states implementation
    
    * change name vrf -> vrfs throughout
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * changes
    
    * push changes
    
    * unit tests running :D
    
    * successful integration tests & unit tests
    
    * changes
    
    * changes
    
    * changes
    
    * sanity test fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * review changes
    
    * WIP vrf_address_family module
    
    * config states
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updates
    
    * update config
    
    * states working
    
    * chore: auto fixes from pre-commit.com hooks
    
    * redundant code rm
    
    * update docs
    
    * update test
    
    * fix lint errors
    
    * review comments
    
    * sanity errors fix
    
    * Update plugins/modules/iosxr_vrf_address_family.py
    
    Co-authored-by: Rohit Thakur <rohitthakur2590@outlook.com>
    
    * review comments
    
    * review comments
    
    * add meta redirect
    
    * review changes
    
    * update
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Rohit Thakur <rohitthakur2590@outlook.com>

--- plugins/module_utils/network/iosxr/facts/facts.py
+++ plugins/module_utils/network/iosxr/facts/facts.py
@@ -94,6 +94,9 @@ from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.sn
 from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.static_routes.static_routes import (
     Static_routesFacts,
 )
+from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.vrf_address_family.vrf_address_family import (
+    Vrf_address_familyFacts,
+)
 from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.vrf_global.vrf_global import (
     Vrf_globalFacts,
 )
@@ -129,6 +132,7 @@ FACT_RESOURCE_SUBSETS = dict(
     snmp_server=Snmp_serverFacts,
     hostname=HostnameFacts,
     bgp_templates=Bgp_templatesFacts,
+    vrf_address_family=Vrf_address_familyFacts,
     vrf_global=Vrf_globalFacts,
 )
 
