857f9f0051f9ce1d63af3e0c0184c725e801db12
KB-perByte <sagpaul@redhat.com>
2024-11-07 16:02:36 +0530

    WIP iosxr vrf_interfaces resouce module

--- plugins/module_utils/network/iosxr/facts/facts.py
+++ plugins/module_utils/network/iosxr/facts/facts.py
@@ -91,6 +91,9 @@ from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.pr
 from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.route_maps.route_maps import (
     Route_mapsFacts,
 )
+from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.vrf_interfaces.vrf_interfaces import (
+    Vrf_interfacesFacts,
+)
 from ansible_collections.cisco.iosxr.plugins.module_utils.network.iosxr.facts.snmp_server.snmp_server import (
     Snmp_serverFacts,
 )
@@ -138,6 +141,7 @@ FACT_RESOURCE_SUBSETS = dict(
     vrf_address_family=Vrf_address_familyFacts,
     vrf_global=Vrf_globalFacts,
     route_maps=Route_mapsFacts,
+    vrf_interfaces=Vrf_interfacesFacts,
 )
 
 
